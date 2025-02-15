bb7548d96f9e62c4b551b99a300838bbf80098c2
Rohan Patil <146317921+rohan7003@users.noreply.github.com>
2025-02-15 16:26:15 +0000

    comparison

---
+++ 01_basics/03_conversionop.js
@@ -0,0 +1,32 @@
+let score = null
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+let isLogedIn = 1
+let booleanisLoggedIn = Boolean(isLogedIn)
+//console.log(booleanisLoggedIn);
+let sNumber = 13
+
+let stringNumber = String(sNumber)
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+//console.log("1"+2);
+//console.log("1"+2);
+//console.log(1+"2");
+//console.log("1"+2+5);
+//console.log(1+5+"7");
+
+console.log(+true);
+console.log(+"");
+
+
---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,14 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);    //=== strictly equal  //== equal  //= assign
+// console.log(null>=0);      // avoid using line 10 to 14
+
+console.log(undefined ==0);
