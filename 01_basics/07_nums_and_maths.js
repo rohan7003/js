74c8ec0cca9fefcb9c4c0717dfa53b154ecd9e16
Rohan Patil <146317921+rohan7003@users.noreply.github.com>
2025-02-25 06:46:09 +0000

    day 7

---
+++ 01_basics/07_nums_and_maths.js
@@ -0,0 +1,41 @@
+const score = 100
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const othernum = 33.283645
+// console.log(othernum.toPrecision(3))
+
+const othernum1 = 353.283645
+// console.log(othernum1.toPrecision(3))
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString());  //american standard
+// console.log(hundreds.toLocaleString('en-IN'));   //indian standard
+
+//           ---------------maths----------------------
+ 
+
+// console.log(Math);
+// console.log(Math.abs(-9));
+// console.log(Math.round(4.8));
+// console.log(Math.floor(4.8));
+// console.log(Math.ceil(4.2));
+// console.log(Math.min(4,3,7,1,9));
+
+console.log(Math.random())
+console.log((Math.random()*10)+1)
+console.log(Math.floor(Math.random()*10)+1)
+
+
+const min =10
+const max =20
+
+// console.log(Math.random()*(max - min + 1));
+console.log(Math.floor(Math.random()*(max - min + 1))+ min);
+
