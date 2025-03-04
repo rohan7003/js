74dac68ff1cff69796e0a6a6fd65004e8b771eea
Rohan Patil <146317921+rohan7003@users.noreply.github.com>
2025-03-04 17:24:23 +0000

    d

---
+++ 01_basics/08_dates.js
@@ -0,0 +1,31 @@
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toDateString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023, 0, 23)     //months start from 0
+// console.log(myCreatedDate.toDateString());
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+// console.log(myCreatedDate.toLocaleString());
+
+// let myCreatedDate = new Date("2023-01-14")    //months start from 1
+let myCreatedDate = new Date("01-14-2023")
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(Date.now()/1000);
+// console.log(Math.floor(Date.now()/1000));
+
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getFullYear());
+console.log(newDate.getMinutes());
+console.log(newDate.getDay());
+
+
+
