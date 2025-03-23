b904507b29cee5b4e76b8e0d0e26929af6d3fda2
Rohan Patil <146317921+rohan7003@users.noreply.github.com>
2025-03-23 10:38:29 +0000

    done

---
+++ 05_iterations/2.js
@@ -0,0 +1,25 @@
+// while
+
+// let index =0
+// while (index<=10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2
+// }
+
+// let myArray = ["batman","spyderman","superman"]
+// let arr = 0
+// while (arr< myArray.length) {
+//     console.log(`Value is ${myArray[arr]}`);
+//     arr = arr+1
+// }
+
+
+
+
+
+// Do-While
+let score = 16
+do {
+    console.log(`score is ${score}`);
+    score ++
+} while (score<=10);
