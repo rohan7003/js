c5b4829d0d29e5a47c2e5a57da9a0557750fc140
Rohan Patil <146317921+rohan7003@users.noreply.github.com>
2025-03-23 06:07:52 +0000

    Done

---
+++ 04_control_flow/true.js
@@ -0,0 +1,38 @@
+// const userEmail = "rp@gmail.com"
+// if (userEmail) {
+//     console.log("Got user email");
+// } else{
+//     console.log("Don't have Email");
+// }
+/*
+Truthy Values = [], "0", "false", " ", {}, function(){} 
+falsy Values = false, 0, -0, BigInt(On), "", null, undefined, NaN */ 
+
+// to check for array
+// const userEmail2 = []
+// if (userEmail2.length === 0) {
+//     console.log("Array is empty");
+// } else{
+//     console.log("Array is not empty");
+// }
+
+// const emptyObj = {}
+//     if (Object.keys(emptyObj).length === 0) {
+//         console.log("Object is empty");
+//     }
+
+// Nullish coalescing Operator (??): null undefined
+// let val1;
+//val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+// val1 = null ?? 72 ?? 34 
+// console.log(val1);
+
+// Terniary Operator
+// condition ? true : false
+
+const iceTestPrice = 100
+iceTestPrice <= 80 ? console.log("less than 80") : console.log("More than 80");
+
+
