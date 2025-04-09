c9d6627f372217ea5951d0c0a1710cab21695b29
Rohan Patil <146317921+rohan7003@users.noreply.github.com>
2025-04-09 16:42:23 +0000

    done

---
+++ 07_projects/02_bimcalculator/code.js
@@ -0,0 +1,26 @@
+const form = document.querySelector('form')
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const height = parseInt(document.querySelector('#height').value);
+  const weight = parseInt(document.querySelector('#weight').value);
+  const results = document.querySelector('#results');
+
+  if (height === '' || height < 0 || isNaN(height)) {
+    results.innerHTML = `Please give a valid height ${height}`;
+  } else if (weight === '' || weight < 0 || isNaN(weight)) {
+    results.innerHTML = `Please give a valid weight ${weight}`;
+  } else {
+    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+    
+    if(results ===  bmi < 18 ){
+      results.innerHTML = `You are Under Weight`
+    }else if(results === bmi > 18.6 && bmi < 24.9) {
+      results.innerHTML = `you are in normal range`
+    }else(results ===  bmi > 25 );{
+      results.innerHTML = `you are over weight`
+    }
+    //show the result
+    results.innerHTML = `<span>${bmi}</span>`;
+  }
+});
